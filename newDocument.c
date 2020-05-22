b8ad2f509db91bdd439e43da738166269abc9859
Mehlam Rafi Khan <hello@mehlamrafikhan.com>
2020-05-22 22:35:40 +0600

    while loop and comment

--- newDocument.c
+++ newDocument.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+/*   int a = 1;
+
+   while(a<=10){
+    printf("Value of a %d\n",a);
+    a++;
+   }    */
+
    return 0;
 }
