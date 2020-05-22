9a90372b9a3bdf06f507385b4a7c3b3dd0de7800
Mehlam Rafi Khan <hello@mehlamrafikhan.com>
2020-05-23 02:03:11 +0600

    if and else added

--- newDocument.c
+++ newDocument.c
@@ -11,9 +11,39 @@ int main() {
 
    //int a;
 
-   for(int a=0; a<=10; a++){
+   /*for(int a=0; a<=10; a++){
     printf("the value of a is %d\n",a);
+   }*/
+
+   int a = 1;
+
+   if(a<=1){
+    printf("a is less than or equel 1");
+   }else{
+    printf("404 not found...");
    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    return 0;
 }
